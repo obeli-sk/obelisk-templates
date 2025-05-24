007a071694f5e77dbfacf0db77c141e560a97417
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-05-24 21:06:49 +0200

    refactor: Simplify fibo activity a bit

--- fibo/activity/src/lib.rs
+++ fibo/activity/src/lib.rs
@@ -7,10 +7,8 @@ export!(Component);
 
 impl Guest for Component {
     fn fibo(n: u8) -> u64 {
-        if n == 0 {
-            0
-        } else if n == 1 {
-            1
+        if n <= 1 {
+            n.into()
         } else {
             Self::fibo(n - 1) + Self::fibo(n - 2)
         }
