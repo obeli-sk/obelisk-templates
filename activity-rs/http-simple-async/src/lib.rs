3c7f979b04170ab76f1ea7a3802c6433d285bbc8
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-06-12 10:21:43 +0200

    feat: Add post example to async client

--- activity-rs/http-simple-async/src/lib.rs
+++ activity-rs/http-simple-async/src/lib.rs
@@ -1,7 +1,8 @@
-use crate::exports::template_http::activity::http_get::Guest;
+use crate::exports::template_http::activity::http_get::Guest as GetGuest;
+use crate::exports::template_http::activity::http_post::Guest as PostGuest;
 use wit_bindgen::generate;
 use wstd::{
-    http::{Client, Method, Request},
+    http::{Client, IntoBody as _, Method, Request},
     runtime::block_on,
 };
 
@@ -11,9 +12,11 @@ export!(Component);
 
 async fn get_plain(url: &str) -> Result<String, anyhow::Error> {
     let client = Client::new();
-    let mut request = Request::builder();
-    request = request.uri(url).method(Method::GET);
-    let request = request.body(wstd::io::empty())?;
+    let request = Request::builder()
+        .uri(url)
+        .method(Method::GET)
+        .header("User-Agent", "my-awesome-agent/1.0")
+        .body(wstd::io::empty())?;
     let response = client.send(request).await?;
     eprintln!("< {:?} {}", response.version(), response.status());
     for (key, value) in response.headers().iter() {
@@ -24,9 +27,57 @@ async fn get_plain(url: &str) -> Result<String, anyhow::Error> {
     Ok(String::from_utf8_lossy(&body).into_owned())
 }
 
-impl Guest for Component {
+async fn get_json(url: &str) -> Result<String, anyhow::Error> {
+    let client = Client::new();
+    let request = Request::builder()
+        .uri(url)
+        .method(Method::GET)
+        .header("Accept", "application/json")
+        .body(wstd::io::empty())?;
+    let response = client.send(request).await?;
+    eprintln!("< {:?} {}", response.version(), response.status());
+    for (key, value) in response.headers().iter() {
+        let value = String::from_utf8_lossy(value.as_bytes());
+        eprintln!("< {key}: {value}");
+    }
+    let body: serde_json::Value = response.into_body().json().await?;
+    Ok(serde_json::to_string(&body)?)
+}
+
+async fn post(url: &str, conetnt_type: &str, body: String) -> Result<String, anyhow::Error> {
+    let client = Client::new();
+    let request = Request::builder()
+        .uri(url)
+        .method(Method::POST)
+        .header("content-type", conetnt_type)
+        .body(body.as_bytes().into_body())?;
+    let response = client.send(request).await?;
+    eprintln!("< {:?} {}", response.version(), response.status());
+    for (key, value) in response.headers().iter() {
+        let value = String::from_utf8_lossy(value.as_bytes());
+        eprintln!("< {key}: {value}");
+    }
+    let body = response.into_body().bytes().await?;
+    Ok(String::from_utf8_lossy(&body).into_owned())
+}
+
+impl GetGuest for Component {
     fn get_plain(url: String) -> Result<String, String> {
         block_on(async move { get_plain(&url).await.map_err(|err| err.to_string()) })
+    }
+
+    fn get_json(url: String) -> Result<String, String> {
+        block_on(async move { get_json(&url).await.map_err(|err| err.to_string()) })
+    }
+}
+
+impl PostGuest for Component {
+    fn post(url: String, content_type: String, body: String) -> Result<String, String> {
+        block_on(async move {
+            post(&url, &content_type, body)
+                .await
+                .map_err(|err| err.to_string())
+        })
     }
 }
 
