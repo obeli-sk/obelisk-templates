faf8288c4050d3817deedcd34f8a193c8bd4e517
Tomas Olvecky <tomas.olvecky@gmail.com>
2026-02-10 16:17:20 +0100

    r: Add `wit-support` generation to `obelisk-generate-wit-dep.sh`, use consistent WIT naming

--- scripts/obelisk-generate-wit-deps.sh
+++ scripts/obelisk-generate-wit-deps.sh
@@ -6,11 +6,13 @@ cd "$(dirname "$0")/.."
 (
 cd fibo/workflow
 rm -rf wit/deps/obelisk_*
+obelisk generate wit-support workflow wit/deps
 obelisk generate wit-deps --overwrite -c obelisk-deps.toml wit/deps
 )
 
 (
 cd fibo/webhook_endpoint
 rm -rf wit/deps/obelisk_*
+obelisk generate wit-support webhook_endpoint wit/deps
 obelisk generate wit-deps --overwrite -c obelisk-deps.toml wit/deps
 )
