7bc659d488e74e5db4b535737bdff929388cacab
Tomáš Olvecký <tomas.olvecky@gmail.com>
2026-04-08 19:12:48 +0200

    chore: Migrate `push-comoponents.sh`

--- scripts/push-components.sh
+++ scripts/push-components.sh
@@ -10,7 +10,7 @@ set -exuo pipefail
 cd "$(dirname "$0")/.."
 
 TAG="$1"
-PREFIX="docker.io/getobelisk"
+PREFIX="oci://docker.io/getobelisk/"
 TMP_DIR=$(mktemp -d -t push-components-XXXXXX)
 trap "rm -rf $TMP_DIR" EXIT
 
@@ -18,7 +18,6 @@ build_and_push() {
     local TEMPLATE="$1"
     local CRATE_NAME="$2"
     local OCI_NAME="$3"
-    local WASM_SUBPATH="$4"
 
     # Redirect to stderr so stdout only contains the OCI URL from `obelisk component push` below,
     # allowing the caller to capture it cleanly via $().
@@ -27,20 +26,18 @@ build_and_push() {
     (
         cd "$TMP_DIR/$CRATE_NAME"
         cargo build --release >&2  # same reason as above
-        obelisk component push "$WASM_SUBPATH" "$PREFIX/$OCI_NAME:$TAG"
+        obelisk component push --deployment obelisk.toml "$CRATE_NAME" "${PREFIX}${OCI_NAME}:${TAG}"
     )
 }
 
 update_toml() {
-    local TOML_COMPONENT_TYPE="$1"
-    local NEW_LOCATION="$2"
-    local COMPONENT_NAME="$3"
-    shift 3
+    local NEW_LOCATION="$1"
+    local COMPONENT_NAME="$2"
+    shift 2
     local TOML_FILES=("$@")
 
     for TOML_FILE in "${TOML_FILES[@]}"; do
-        obelisk component add "$TOML_COMPONENT_TYPE" "$NEW_LOCATION" \
-            --name "$COMPONENT_NAME" --deployment "$TOML_FILE"
+        obelisk component add --deployment "$TOML_FILE" "$NEW_LOCATION" "$COMPONENT_NAME"
     done
 }
 
@@ -48,10 +45,9 @@ update_toml() {
 ACTIVITY_LOCATION=$(build_and_push \
     fibo/activity \
     activity_myfibo \
-    example_activity_fibo_template \
-    "target/wasm32-wasip2/release/activity_myfibo.wasm")
+    example_activity_fibo_template)
 
-update_toml activity_wasm "$ACTIVITY_LOCATION" activity_myfibo \
+update_toml "$ACTIVITY_LOCATION" activity_myfibo \
     fibo/workflow/obelisk.toml \
     fibo/webhook_endpoint/obelisk.toml \
     fibo/workflow/obelisk-deps.toml \
@@ -61,10 +57,9 @@ update_toml activity_wasm "$ACTIVITY_LOCATION" activity_myfibo \
 WORKFLOW_LOCATION=$(build_and_push \
     fibo/workflow \
     workflow_myfibo \
-    example_workflow_fibo_template \
-    "target/wasm32-unknown-unknown/release/workflow_myfibo.wasm")
+    example_workflow_fibo_template)
 
-update_toml workflow_wasm "$WORKFLOW_LOCATION" workflow_myfibo \
+update_toml "$WORKFLOW_LOCATION" workflow_myfibo \
     fibo/webhook_endpoint/obelisk.toml \
     fibo/webhook_endpoint/obelisk-deps.toml
 
