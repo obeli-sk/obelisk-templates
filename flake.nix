c2534ed6d692527678d00ddaeb341278496b31fa
Tomáš Olvecký <tomas.olvecky@gmail.com>
2026-05-25 19:09:00 +0200

    chore: Upgrade to obelisk 0.38.0-rc.2

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       };
     };
     obelisk = {
-      url = "github:obeli-sk/obelisk/latest";
+      url = "github:obeli-sk/obelisk/prerelease";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
